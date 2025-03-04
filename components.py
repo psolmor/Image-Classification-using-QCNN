90ef932a4a9e1f97b23306a68550852da02c9989
psolmor <pedro.solermor@gmail.com>
2025-03-04 10:56:56 +0100

    Add convolutional circuit

---
+++ components.py
@@ -0,0 +1,12 @@
+import pennylane as qml
+
+
+def pooling_circuit(params, wires): #2 params
+    qml.CRZ(params[0], wires=[wires[0], wires[1]])
+    qml.PauliX(wires=wires[0])
+    qml.CRX(params[1], wires=[wires[0], wires[1]])
+
+def U_TTN(params, wires):  # 2 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
