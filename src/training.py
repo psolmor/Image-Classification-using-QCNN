27366c68ef7826a2d343157182b530aaf1017872
psolmor <pedro.solermor@gmail.com>
2025-03-05 20:40:33 +0100

    Fix loss func

--- src/training.py
+++ src/training.py
@@ -6,6 +6,6 @@ import autograd.numpy as anp
 def square_loss(labels,predictions):
     loss=0
     for l,p in zip(labels,predictions):
-        loss+=(1-p)**2
+        loss+=(l-p)**2
     loss=loss/len(labels)
     return loss
