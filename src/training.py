b1f37941de23dbba6219f349aa5df73f3481293b
psolmor <pedro.solermor@gmail.com>
2025-03-05 20:43:39 +0100

    Add cost func

--- src/training.py
+++ src/training.py
@@ -1,4 +1,4 @@
-import circuit
+import circuit as cir
 import pennylane as qml
 import numpy as np
 import autograd.numpy as anp
@@ -8,4 +8,11 @@ def square_loss(labels,predictions):
     for l,p in zip(labels,predictions):
         loss+=(l-p)**2
     loss=loss/len(labels)
+    return loss
+
+def cost(params, X, Y, U, U_params,circuit):
+  
+    predictions = [cir.QCNN(x, params, U, U_params) for x in X]
+    loss = square_loss(Y, predictions)
+
     return loss
