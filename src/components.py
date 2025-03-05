5cde9eb01cd4528851a5ec7b5aefd6503af4f69c
psolmor <pedro.solermor@gmail.com>
2025-03-05 20:07:47 +0100

    Add circuits for each layer

--- src/components.py
+++ src/components.py
@@ -10,3 +10,34 @@ def U_TTN(params, wires):  # 2 params
     qml.RY(params[0], wires=wires[0])
     qml.RY(params[1], wires=wires[1])
     qml.CNOT(wires=[wires[0], wires[1]])
+
+#Circuits for conv layers
+def conv_layer1(U, params):
+    U(params, wires=[0, 7])
+
+    for i in range(0, 8, 2):
+        U(params, wires=[i, i + 1])
+    for i in range(1, 7, 2):
+        U(params, wires=[i, i + 1])
+
+def conv_layer2(U, params):
+    U(params, wires=[0, 6])
+
+    U(params, wires=[0, 2])
+    U(params, wires=[4, 6])
+    U(params, wires=[2, 4])
+
+def conv_layer3(U, params):
+    U(params, wires=[0,4])
+
+# Quantum Circuits for Pooling layers
+def pooling_layer1(V, params):
+    for i in range(0, 8, 2):
+        V(params, wires=[i + 1, i])
+
+def pooling_layer2(V, params):
+    V(params, wires=[2,0])
+    V(params, wires=[6,4])
+    
+def pooling_layer3(V, params):
+    V(params, wires=[0,4])
