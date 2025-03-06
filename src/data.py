20fdf1b34c6119ed43a8922dc25d59d42b441cd6
psolmor <pedro.solermor@gmail.com>
2025-03-06 14:09:39 +0100

    Change dataset to take only number 1 and 0

--- src/data.py
+++ src/data.py
@@ -1,22 +1,48 @@
 import numpy as np 
 import tensorflow as tf 
+import matplotlib.pyplot as plt 
 
 
-#dataset is a string to indicate which data are we using
-def data_load_and_process(dataset="mnist"):
+def data_load_and_process():
     
     print("Loading data...")
-    if dataset=="fashion_mnist":
-        (x_train,y_train),(x_test,y_test)=tf.keras.datasets.fashion_mnist.load_data()
-    elif dataset=="mnist":
-        (x_train,y_train),(x_test,y_test)=tf.keras.datasets.mnist.load_data()
-   
+    (x_train,y_train),(x_test,y_test)=tf.keras.datasets.mnist.load_data()
+
+
+    print("Filtering data...")
+    x_train_filtered = []
+    y_train_filtered = []
+    for i in range(len(y_train)):
+        if y_train[i] == 0 or y_train[i] == 1:
+            x_train_filtered.append(x_train[i])
+            y_train_filtered.append(y_train[i])
+
+    x_test_filtered = []
+    y_test_filtered = []
+    for i in range(len(y_test)):
+        if y_test[i] == 0 or y_test[i] == 1:
+            x_test_filtered.append(x_test[i])
+            y_test_filtered.append(y_test[i])
+
+    x_train = np.array(x_train_filtered)
+    y_train = np.array(y_train_filtered)
+    x_test = np.array(x_test_filtered)
+    y_test = np.array(y_test_filtered)
+
+    """"
+    fig, axes = plt.subplots(1, 4, figsize=(10, 3))
+    for i, ax in enumerate(axes):
+        ax.imshow(x_train[i].reshape(28, 28), cmap='gray')
+        ax.set_title(f"Label: {y_train[i]}")
+        ax.axis('off')
+    plt.show()
+    """
+
     print("Data loaded, normalization...")    
 
     x_train, x_test = x_train[..., np.newaxis] / 255.0, x_test[..., np.newaxis] / 255.0  
 
-    y_train = np.where(y_train > 4, 1, -1)
-    y_test = np.where(y_test > 4, 1, -1)
+
 
     print("Resizing image with linear interpolation and flattening vector...")
 
@@ -26,3 +52,4 @@ def data_load_and_process(dataset="mnist"):
     print("Data is ready")
 
     return x_train, x_test, y_train, y_test
+
