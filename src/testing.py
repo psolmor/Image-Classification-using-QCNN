9b1fe7a9f8e914759ed6f1e2537f8965cd7e7955
psolmor <pedro.solermor@gmail.com>
2025-03-14 12:50:12 +0100

    Add counter of testing image

--- src/testing.py
+++ src/testing.py
@@ -2,8 +2,11 @@ import numpy as np
 
 def accuracy_test(predictions, labels):
     acc = 0
+    counter=0
     for l, p in zip(labels, predictions):
         if np.abs(l - p) < 1:
             acc = acc + 1
+        if counter%10==0:
+            print(f"Testing image: {counter}/{len(predictions)}")
     return acc / len(labels)
 
