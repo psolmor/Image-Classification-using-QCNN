c4e6e4d7058186ee1bac2289fe9e5550f57e5928
psolmor <pedro.solermor@gmail.com>
2025-03-26 19:37:12 +0100

    Add comparison between original image and resized one

--- convolution_example/convolution_example.py
+++ convolution_example/convolution_example.py
@@ -2,22 +2,45 @@ import numpy as np
 import tensorflow as tf 
 import matplotlib.pyplot as plt 
 
-def plot_images(image_array):
-    figures, axis = plt.subplots(3, 3, figsize=(8, 8))
-    for i in range(3):
-        for j in range(3):
-            idx = i * 3 + j  
-            axis[i, j].imshow(image_array[idx], cmap="gray") 
-            axis[i, j].axis("off") 
+def plot_comparison(original, resized, title="Comparison of Images"):
+    num_images = len(original)
+    fig, axes = plt.subplots(num_images, 2, figsize=(8, 4 * num_images))
+
+    for i in range(num_images):
+        # Mostrar imagen original
+        axes[i, 0].imshow(original[i], cmap="gray")
+        axes[i, 0].set_title("Original (28x28)")
+        axes[i, 0].axis("off")
+
+        # Mostrar imagen redimensionada
+        axes[i, 1].imshow(resized[i], cmap="gray")
+        axes[i, 1].set_title("Resized (16x16)")
+        axes[i, 1].axis("off")
+
+    plt.suptitle(title)
+    plt.tight_layout()
+    plt.savefig("Original vs Resized")
     plt.show()
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+x_train_sample = np.array([x_train[i] for i in range(3)])
 
-x_train_sample = [x_train[i] for i in range(9)]
-plot_images(x_train_sample)
+x_train_sample_normalized = x_train_sample / 255.0
 
 
-x_train = x_train[..., np.newaxis] / 255.0
-x_train = tf.image.resize(x_train[:], (256, 1), method="bilinear").numpy().squeeze()
+x_train_sample_expanded = np.expand_dims(x_train_sample_normalized, axis=-1)
 
+
+x_train_sample_resized = tf.image.resize(x_train_sample_expanded, (16, 16), method="bilinear").numpy()
+
+
+x_train_sample_resized = np.squeeze(x_train_sample_resized, axis=-1)
+
+
+np.save("resized_images.npy", x_train_sample_resized)
+
+
+plot_comparison(x_train_sample, x_train_sample_resized, "Original vs Resized")
+
+print("Imágenes redimensionadas guardadas como 'resized_images.npy'")
