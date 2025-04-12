0790a541faa3cd42f403d8481b02f03e5fb6914c
psolmor <pedro.solermor@gmail.com>
2025-04-13 00:19:23 +0200

    Add area resize

--- convolution_example/convolution_example.py
+++ convolution_example/convolution_example.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt 
 from sklearn.decomposition import PCA
 
-def plot_comparison(original, bilinear, nearest, title="Comparison of Images"):
+def plot_comparison(original, bilinear, nearest,area, title="Comparison of Images"):
     num_images = len(original)
-    fig, axes = plt.subplots(3, num_images, figsize=(8, 4 * num_images))
+    fig, axes = plt.subplots(4, num_images, figsize=(8, 4 * num_images))
 
     for i in range(num_images):
         # Mostrar imagen original
@@ -22,6 +22,11 @@ def plot_comparison(original, bilinear, nearest, title="Comparison of Images"):
         axes[2, i].imshow(nearest[i], cmap="gray")
         axes[2, i].set_title("Nearest Resized (16x16)")
         axes[2, i].axis("off")
+
+        # Mostrar imagen redimensionada con interpolación nearest
+        axes[3, i].imshow(area[i], cmap="gray")
+        axes[3, i].set_title("Area Resized (16x16)")
+        axes[3, i].axis("off")
 
     plt.suptitle(title)
     plt.tight_layout()
@@ -43,6 +48,10 @@ x_train_sample_near_expanded = np.expand_dims(x_train_sample_normalized, axis=-1
 x_train_sample_near_resized = tf.image.resize(x_train_sample_near_expanded, (16, 16), method="nearest").numpy()
 x_train_sample_near_resized = np.squeeze(x_train_sample_near_resized, axis=-1)
 
+x_train_sample_area_expanded = np.expand_dims(x_train_sample_normalized, axis=-1)
+x_train_sample_area_resized = tf.image.resize(x_train_sample_area_expanded, (16, 16), method="area").numpy()
+x_train_sample_area_resized = np.squeeze(x_train_sample_area_resized, axis=-1)
 
-plot_comparison(x_train_sample, x_train_sample_bil_resized,x_train_sample_near_resized, "Original vs Resized")
+
+plot_comparison(x_train_sample, x_train_sample_bil_resized,x_train_sample_near_resized,x_train_sample_area_resized, "Original vs Resized")
 
