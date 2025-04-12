bc52207c207328c650222f9920d0ffc86f249da5
psolmor <pedro.solermor@gmail.com>
2025-04-12 22:45:22 +0200

    Add nearest Resize Image

--- convolution_example/convolution_example.py
+++ convolution_example/convolution_example.py
@@ -1,10 +1,11 @@
 import numpy as np 
 import tensorflow as tf 
 import matplotlib.pyplot as plt 
+from sklearn.decomposition import PCA
 
-def plot_comparison(original, resized, title="Comparison of Images"):
+def plot_comparison(original, bilinear, nearest, title="Comparison of Images"):
     num_images = len(original)
-    fig, axes = plt.subplots(2, num_images, figsize=(8, 4 * num_images))
+    fig, axes = plt.subplots(3, num_images, figsize=(8, 4 * num_images))
 
     for i in range(num_images):
         # Mostrar imagen original
@@ -12,14 +13,19 @@ def plot_comparison(original, resized, title="Comparison of Images"):
         axes[0, i].set_title("Original (28x28)")
         axes[0, i].axis("off")
 
-        # Mostrar imagen redimensionada
-        axes[1, i].imshow(resized[i], cmap="gray")
-        axes[1, i].set_title("Resized (16x16)")
+        # Mostrar imagen redimensionada con interpolación bilineal
+        axes[1, i].imshow(bilinear[i], cmap="gray")
+        axes[1, i].set_title("Bilinear Resized (16x16)")
         axes[1, i].axis("off")
+
+        # Mostrar imagen redimensionada con interpolación nearest
+        axes[2, i].imshow(nearest[i], cmap="gray")
+        axes[2, i].set_title("Nearest Resized (16x16)")
+        axes[2, i].axis("off")
 
     plt.suptitle(title)
     plt.tight_layout()
-    plt.savefig("Original vs Resized")
+    plt.savefig("Original_vs_Resized.png")
     plt.show()
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -29,18 +35,14 @@ x_train_sample = np.array([x_train[i] for i in range(3)])
 x_train_sample_normalized = x_train_sample / 255.0
 
 
-x_train_sample_expanded = np.expand_dims(x_train_sample_normalized, axis=-1)
+x_train_sample_bil_expanded = np.expand_dims(x_train_sample_normalized, axis=-1)
+x_train_sample_bil_resized = tf.image.resize(x_train_sample_bil_expanded, (16, 16), method="bilinear").numpy()
+x_train_sample_bil_resized = np.squeeze(x_train_sample_bil_resized, axis=-1)
+
+x_train_sample_near_expanded = np.expand_dims(x_train_sample_normalized, axis=-1)
+x_train_sample_near_resized = tf.image.resize(x_train_sample_near_expanded, (16, 16), method="nearest").numpy()
+x_train_sample_near_resized = np.squeeze(x_train_sample_near_resized, axis=-1)
 
 
-x_train_sample_resized = tf.image.resize(x_train_sample_expanded, (16, 16), method="bilinear").numpy()
+plot_comparison(x_train_sample, x_train_sample_bil_resized,x_train_sample_near_resized, "Original vs Resized")
 
-
-x_train_sample_resized = np.squeeze(x_train_sample_resized, axis=-1)
-
-
-np.save("resized_images.npy", x_train_sample_resized)
-
-
-plot_comparison(x_train_sample, x_train_sample_resized, "Original vs Resized")
-
-print("Imágenes redimensionadas guardadas como 'resized_images.npy'")
