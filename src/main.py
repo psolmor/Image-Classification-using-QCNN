a18f1c325530f1825d337b7e8c46e09a72f6c8d6
psolmor <pedro.solermor@gmail.com>
2025-04-13 20:48:52 +0200

    Create utilities file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import benchmark
+import utils
 
 #Here the nechmark is set up by hardcodding the desired parameters
 
 number_pairs = [(0, 1)] # [(0, 1),(2,3)...]
 unitary_circuit=["CONV"] # "TTN" "CONV"
 resize=["area","bilinear","nearest"] # "area" "bilinear" "nearest"
-params_num=15 # TTN=12 CONV=15
+params_num=utils.param_num(resize) # TTN=12 CONV=15
 embedding="Amplitude"
 
 benchmark.benchmark(number_pairs,unitary_circuit,embedding,resize)
---
+++ src/utils.py
@@ -0,0 +1,6 @@
+def param_num(circuit):
+    if circuit=="CONV":
+        number=15
+    elif circuit=="TTN":
+        number=13
+    return number
