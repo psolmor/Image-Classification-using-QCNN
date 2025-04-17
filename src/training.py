7ee33ea46d6a8bda20a87b871fcc00fe0388af9e
psolmor <pedro.solermor@gmail.com>
2025-04-17 11:27:04 +0200

    Refactori

--- src/training.py
+++ src/training.py
@@ -1,50 +1,37 @@
-import circuit
 import pennylane.numpy as pnp
-from pennylane.optimize import NesterovMomentumOptimizer,AdamOptimizer
-import utils
+import numpy as np 
+import pennylane as qml
 
-#Loss Functions
+import utils
+import circuit
+
+def circuit_training(X_train, Y_train, unitary, embedding):
+
+    num_params = utils.param_num(unitary)
+    params = pnp.array(np.random.uniform(0, 2*np.pi, size=num_params),requires_grad=True)
+    
+    opt = qml.NesterovMomentumOptimizer(stepsize=0.01)
+
+    for it in range(200):
+    
+        batch_indices = np.random.randint(0, len(X_train), size=25)
+        X_batch = X_train[batch_indices]
+        Y_batch = Y_train[batch_indices]
+        
+        params, cost_new = opt.step_and_cost(
+            lambda p: cost(p, X_batch, Y_batch, unitary, embedding),
+            params
+        )
+        
+        if it % 10 == 0:
+            print(f"Iteration {it}: Cost = {cost_new:.4f}")
+    
+    return params
+
+def cost(params, X, Y, unitary, embedding):
+    predictions = pnp.array([circuit.QCNN(x, params, unitary, embedding) for x in X])
+    return square_loss(Y, predictions)
 
 def square_loss(labels, predictions):
-    loss = 0
-    for l, p in zip(labels, predictions):
-        loss = loss + (l - p) ** 2
-    loss = loss / len(labels)
+    loss = pnp.mean((labels - predictions) ** 2)
     return loss
-
-#Cost function
-def cost(params, X, Y,unitary,embedding):
-  
-    predictions = [circuit.QCNN(x,params,unitary,embedding) for x in X]
-    loss = square_loss(Y, predictions)
-
-    return loss
-
-
-
-#Training parameters
-steps=200
-learning_rate=0.01
-batch_size=25
-
-#Function in charge to optimize the circuit
-def circuit_training(X_train, Y_train,unitary,embedding):
-       
-    params = pnp.array(pnp.random.randn(utils.param_num(unitary)), requires_grad=True)
-    #opt = AdamOptimizer(stepsize=learning_rate)
-    opt=NesterovMomentumOptimizer(stepsize=learning_rate) 
-    
-    def cost_fn(params):
-        batch_index = pnp.random.randint(0, len(X_train), batch_size)
-        X_batch = [X_train[i] for i in batch_index]
-        Y_batch = [Y_train[i] for i in batch_index]
-        return cost(params, X_batch, Y_batch,unitary,embedding)
-
-
-    for it in range(steps):
-        params, cost_new = opt.step_and_cost(cost_fn,params)
-
-        if it % 10 == 0:
-            print("iteration: ", it, " cost: ", cost_new)
-
-    return  params
