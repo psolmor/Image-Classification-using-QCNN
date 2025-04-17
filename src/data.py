19e2088e17403c17018b0e5c58b72f9b08d29caa
psolmor <pedro.solermor@gmail.com>
2025-04-17 11:27:58 +0200

    Remove prints in data

--- src/data.py
+++ src/data.py
@@ -11,7 +11,7 @@ def data_load_and_process(class1,class2,resize,dataset="mnist"):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 
-    print("Filtering data...")
+    #print("Filtering data...")
     x_train_filtered = []
     y_train_filtered = []
     for i in range(len(y_train)):
@@ -34,13 +34,13 @@ def data_load_and_process(class1,class2,resize,dataset="mnist"):
     y_train = [1 if y==class1 else -1 for y in y_train]
     y_test = [1 if y==class1 else -1 for y in y_test]
 
-    print("Data loaded, normalization...")    
+   #print("Data loaded, normalization...")    
 
     x_train, x_test = x_train[..., np.newaxis] / 255.0, x_test[..., np.newaxis] / 255.0  
 
 
 
-    print(f"Resizing image using {resize} resizing and flattening vector...")
+    #print(f"Resizing image using {resize} resizing and flattening vector...")
 
     if resize=="bilinear":
         x_train = tf.image.resize(x_train[:], (256, 1), method="bilinear").numpy().squeeze()
