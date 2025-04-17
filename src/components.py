e517fa99009cbcfc7dc89eac3d9f5a81a12c2fb2
psolmor <pedro.solermor@gmail.com>
2025-04-17 10:55:38 +0200

    Modify circuitsd + doocstrings

--- src/components.py
+++ src/components.py
@@ -1,27 +1,27 @@
 import pennylane as qml
 import numpy as np
 
+def conv_circuit(params, wires):
+    """Convolutional Circuit #3 params"""
+    qml.RZ(params[0], wires=wires[0])
+    qml.RZ(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RZ(params[2], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
 
-def pooling_circuit(params, wires): #2 params
-    qml.CRZ(params[0], wires=[wires[0], wires[1]])
-    qml.PauliX(wires=wires[0])
-    qml.CRX(params[1], wires=[wires[0], wires[1]])
+def pool_circuit(params, wires):
+    """Pooling Circuit #3 params"""
+    qml.RZ(-np.pi/2, wires=wires[1])
+    qml.CNOT(wires=[wires[1], wires[0]])
+    qml.RZ(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(params[2], wires=wires[1])
 
-def U_TTN(params, wires):  # 2 params
+def U_TTN(params, wires):
+    """Simplified Convolutional Circuit #3 params"""
     qml.RY(params[0], wires=wires[0])
     qml.RY(params[1], wires=wires[1])
-    qml.CNOT(wires=[wires[0], wires[1]]) 
-
-def U_CONV(params,wires): # 3 params
-    qml.RZ(-np.pi/2,wires=wires[1])
-    qml.CNOT(wires=[wires[1], wires[0]])
-    qml.RZ(params[0],wires=wires[0])
-    qml.RY(params[1],wires=wires[1])
     qml.CNOT(wires=[wires[0], wires[1]])
-    qml.RY(params[2],wires=wires[1])
-    qml.CNOT(wires=[wires[1], wires[0]])
-    qml.RZ(np.pi/2,wires=wires[0])
-
-
 
 
