0a6fe77940b907c826b3056b3a773c14bcd2e17b
psolmor <pedro.solermor@gmail.com>
2025-05-23 01:22:09 +0200

    Add quality of life

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ dataset = "fashion_mnist"  # 'fashion_mnist' o "mnist"
 class_pairs = [(0,1)]# [(0, 1),(2,3)...]
 unitary_circuit = ["TTN"]  # "TTN" o "CONV"
 resize = ["nearest"]  # "area", "bilinear" o "nearest"
-embedding = "Amplitude"
+embedding = "angle" if "autoencoder" in resize else "amplitude"
+print(embedding)
 
 benchmark.benchmark(class_pairs, unitary_circuit, embedding, resize, dataset,iterations=1)
